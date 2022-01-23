9098c4339e120bba02120eac2ae2db5791108346
curite <onthepound@gmail.com>
2022-01-23 21:28:03 +0900

    キーボード部分を変更 数字および記号が４ｘ４のGridViewで表示されるように変更

--- lib/keyboard_area.dart
+++ lib/keyboard_area.dart
@@ -2,16 +2,38 @@
 import 'package:flutter/material.dart';
 
 class KeyboardArea extends StatelessWidget {
+  // @formatter:off
+  var keyboardLabels = [
+    '7', '8', '9', '÷',
+    '4', '5', '6', '×',
+    '1', '2', '3', '-',
+    'C', '0', '=', '+',
+  ]; // @formatter:on
+
   @override
   Widget build(BuildContext context) {
-    return Text('Keyboard');
+    return Expanded(
+      flex: 2,
+      child: GridView.count(
+        crossAxisCount: 4,
+        children: keyboardLabels.map((keyLabel) {
+          return GridTile(
+            child: Button(keyLabel),
+          );
+        }).toList(),
+      ),
+    );
   }
 }
 
 //ボタン部分
 class Button extends StatelessWidget {
+  var _keyLabel;
+
+  Button(this._keyLabel);
+
   @override
   Widget build(BuildContext context) {
-    return Text('Botton');
+    return Text(_keyLabel);
   }
 }
