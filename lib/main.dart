28794c9848250892fe1fcaf0ef8b0412cf2a3d46
curite <onthepound@gmail.com>
2022-01-25 13:24:02 +0900

    デザインサイズの暗黙キャストを修正

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(360, 690),
+      designSize: const Size(360.0, 690.0),
       minTextAdapt: true,
       splitScreenMode: true,
       builder: () => MaterialApp(
