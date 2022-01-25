431e4314c5d340189b20f3f01bc18c8121964dc9
curite <onthepound@gmail.com>
2022-01-25 15:53:37 +0900

    監視したい変数の専用コントローラを作成 コントローラの変数を扱えるようにGet.putで宣言

--- lib/display_area.dart
+++ lib/display_area.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
+class DisplayController extends GetxController {
+  final Rx<String> _displayExpression = '1+1'.obs;
+}
+
 //ディスプレイ部分
 class DisplayArea extends StatefulWidget {
   const DisplayArea({Key? key}) : super(key: key);
@@ -11,15 +15,15 @@ class DisplayArea extends StatefulWidget {
 }
 
 class _DisplayAreaState extends State<DisplayArea> {
-  final Rx<String> _displayExpression = '1+1'.obs;
+  final DisplayController displayController = Get.put(DisplayController());
 
   // ディスプレイの再描画処理
   _UpdateDisplay(String letter) {
     setState(() {
       if (letter == '=' || letter == 'C') {
-        _displayExpression.value = '';
+        displayController._displayExpression.value = '';
       } else {
-        _displayExpression.value += letter;
+        displayController._displayExpression.value += letter;
       }
     });
   }
@@ -31,7 +35,7 @@ class _DisplayAreaState extends State<DisplayArea> {
       child: Container(
         alignment: Alignment.centerRight,
         child: Text(
-          _displayExpression.value,
+          displayController._displayExpression.value,
           style: TextStyle(
             fontSize: 64.0.sp,
           ),
