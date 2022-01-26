9930c641c9b64fca33c2b17ec8635307432d51d9
curite <onthepound@gmail.com>
2022-01-26 09:41:46 +0900

    処理の大枠を作成

--- lib/calculation.dart
+++ lib/calculation.dart
@@ -1,1 +1,14 @@
-class Calculation {}
+class Calculation {
+  var number = [];
+  var operator = [];
+
+  static void getKey(String letter) {
+    // TODO
+  }
+
+  static String execute() {
+    int result = 0;
+    // TODO
+    return result.toString();
+  }
+}
