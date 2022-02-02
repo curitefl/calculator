3f4ac54624b6bf58b7f0e38ff5fe8202209b6e06
curite <onthepound@gmail.com>
2022-02-02 16:25:00 +0900

    文字列をTextDataクラスで定義

--- lib/text_data.dart
+++ lib/text_data.dart
@@ -8,4 +8,10 @@ class TextData {
   static const String empty = '';
   static const String zero = '0';
   static const String decimalPoint = '.';
+  static const String plus = '+';
+  static const String minus = '-';
+  static const String multiply = '×';
+  static const String divide = '÷';
+  static const String checkNumber = '0123456789';
+  static const String checkOperator = '+-×÷';
 }
