0abc4dd7b877c3f77a958082300d026284bb7d38
curite <onthepound@gmail.com>
2022-02-04 20:52:31 +0900

    if文をswitch文に変更

--- lib/display_controller.dart
+++ lib/display_controller.dart
@@ -22,46 +22,47 @@ class DisplayController extends GetxController {
 
   // ディスプレイの再描画処理
   void updateDisplay(String letter) {
-    // Cキーの場合
-    if (letter == TextData.clearKey) {
-      displayExpression.value = TextData.empty;
-      Calculation.resultFlag = false;
-    }
-    // =キーの場合
-    else if (letter == TextData.equalKey) {
-      displayExpression.value = Calculation.execute(
-          displayExpression.value, displayExpression.value.length);
-    }
-    // エラーの場合
-    else if (letter == TextData.error) {
-      displayExpression.value = TextData.error;
-      Calculation.resultFlag = false;
-    }
-    // 数字、四則演算子の場合
-    else {
-      // 画面が空ではなく
-      if (displayExpression.value != TextData.empty) {
-        final int last = displayExpression.value.length - 1;
-        // 画面末尾が四則演算子かつ、四則演算子が入力されたら、入力された演算子に置き換える
-        if (TextData.allOperators.contains(displayExpression.value[last]) &&
-            TextData.allOperators.contains(letter)) {
-          replaceOperator(letter);
-          return;
+    switch (letter) {
+      // Cキーの場合
+      case TextData.clearKey:
+        displayExpression.value = TextData.empty;
+        Calculation.resultFlag = false;
+        break;
+      // =キーの場合
+      case TextData.equalKey:
+        displayExpression.value = Calculation.execute(
+            displayExpression.value, displayExpression.value.length);
+        break;
+      // エラーの場合
+      case TextData.error:
+        displayExpression.value = TextData.error;
+        Calculation.resultFlag = false;
+        break;
+      // 数字、四則演算子の場合
+      default:
+        // 画面が空ではなく
+        if (displayExpression.value != TextData.empty) {
+          final int last = displayExpression.value.length - 1;
+          // 画面末尾が四則演算子かつ、四則演算子が入力されたら、入力された演算子に置き換える
+          if (TextData.allOperators.contains(displayExpression.value[last]) &&
+              TextData.allOperators.contains(letter)) {
+            replaceOperator(letter);
+            return;
+          }
         }
-      }
-      // 結果画面で
-      if (Calculation.resultFlag == true) {
-        // 数字が入力されたら、画面とFlagを空にする
-        if (TextData.allNumbers.contains(letter)) {
-          displayExpression.value = TextData.empty;
-          Calculation.resultFlag = false;
+        // 結果画面で
+        if (Calculation.resultFlag == true) {
+          // 数字が入力されたら、画面とFlagを空にする
+          if (TextData.allNumbers.contains(letter)) {
+            displayExpression.value = TextData.empty;
+            Calculation.resultFlag = false;
+          }
+          // 四則演算子が入力されたら、Flagを空にする
+          else if (TextData.allOperators.contains(letter)) {
+            Calculation.resultFlag = false;
+          }
         }
-        // 四則演算子が入力されたら、Flagを空にする
-        else if (TextData.allOperators.contains(letter)) {
-          Calculation.resultFlag = false;
-        }
-      }
-      displayExpression.value += letter;
+        displayExpression.value += letter;
     }
   }
 
