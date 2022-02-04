c25ad8e98d2fee468879798ca898a07b5aba0ad5
curite <onthepound@gmail.com>
2022-02-04 19:08:46 +0900

    コメントアウトしている箇所以外のテストを個々に実行すれば、成功するところまで確認

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,121 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+import 'package:calculator/calculation.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:calculator/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  group('Calculation Test', () {
+    test('1+1=', () {
+      var expression = '1+1';
+      var length = expression.length;
+      var result = Calculation.execute(expression, length);
+      expect('2', result);
+      Calculation.clearVariable();
+    });
+    test('1-1-10=', () {
+      var expression = '1-1-10';
+      var length = expression.length;
+      var result = Calculation.execute(expression, length);
+      expect('-10', result);
+    });
+    test('2×3×5=', () {
+      var expression = '2×3×5';
+      var length = expression.length;
+      var result = Calculation.execute(expression, length);
+      expect('30', result);
+    });
+    test('60÷2÷3=', () {
+      var expression = '60÷2÷3';
+      var length = expression.length;
+      var result = Calculation.execute(expression, length);
+      expect('10', result);
+    });
+    test('100÷5+10×2-5', () {
+      var expression = '100÷5+10×2-5';
+      var length = expression.length;
+      var result = Calculation.execute(expression, length);
+      expect('55', result);
+    });
+    // test('9-+×÷3=', () {
+    //   var expression = '9-+×÷3';
+    //   var length = expression.length;
+    //   var result = Calculation.execute(expression, length);
+    //   expect('3', result);
+    // });
+    test('1+1====', () {
+      var expression = '1+1';
+      var expects = ['2', '3', '4', '5'];
+      var equalCount = 4;
+      for (var i = 0; i < equalCount; ++i) {
+        var length = expression.length;
+        expression = Calculation.execute(expression, length);
+        var result = expression;
+        expect(expects[i], result);
+      }
+    });
+    test('2×2====', () {
+      var expression = '2×2';
+      var expects = ['4', '8', '16', '32'];
+      var equalCount = 4;
+      for (var i = 0; i < equalCount; ++i) {
+        var length = expression.length;
+        expression = Calculation.execute(expression, length);
+        var result = expression;
+        expect(expects[i], result);
+      }
+    });
+    test('1024÷2====', () {
+      var expression = '1024÷2';
+      var expects = ['512', '256', '128', '64'];
+      var equalCount = 4;
+      for (var i = 0; i < equalCount; ++i) {
+        var length = expression.length;
+        expression = Calculation.execute(expression, length);
+        var result = expression;
+        expect(expects[i], result);
+      }
+    });
+    test('50-5====', () {
+      var expression = '50-5';
+      var expects = ['45', '40', '35', '30'];
+      var equalCount = 4;
+      for (var i = 0; i < equalCount; ++i) {
+        var length = expression.length;
+        expression = Calculation.execute(expression, length);
+        var result = expression;
+        expect(expects[i], result);
+      }
+    });
+    // test('5+=+=+=+=', () {
+    //   var expression = '5+';
+    //   var expects = ['10', '20', '40', '80'];
+    //   var equalCount = 2;
+    //   for (var i = 0; i < equalCount; ++i) {
+    //     var length = expression.length;
+    //     expression = Calculation.execute(expression, length);
+    //     var result = expression;
+    //     expression = result + '+';
+    //     expect(expects[i], result);
+    //   }
+    // });
+    // test('2×=×=×=×=', () {
+    //   var expression = '2×=×=×=×';
+    //   var length = expression.length;
+    //   var result = Calculation.execute(expression, length);
+    //   expect('4, 16, 256, 65536', result);
+    // });
+    // test('256÷=÷=÷=÷=', () {
+    //   var expression = '256÷=÷=÷=÷';
+    //   var length = expression.length;
+    //   var result = Calculation.execute(expression, length);
+    //   expect('1, 1, 1, 1', result);
+    // });
+    test('32÷0', () {
+      var expression = '32÷0';
+      var length = expression.length;
+      var result = Calculation.execute(expression, length);
+      expect('Error', result);
+    });
   });
 }
